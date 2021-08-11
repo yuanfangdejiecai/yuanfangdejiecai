d6698d3a22995d37262e021a24318f0f0acf9e17
远方 <1635886852@qq.com>
2021-08-12 00:38:59 +0800

    提交更改

--- my_vue_project/src/main.js
+++ my_vue_project/src/main.js
@@ -3,4 +3,41 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+const app = createApp(App);
+
+const requireComponent = require.context(
+  // 其组件目录的相对路径
+  "@/components",
+  // 是否查询其子目录
+  true,
+  // 匹配基础组件文件名的正则表达式
+  /\.vue$/
+);
+requireComponent.keys().forEach((fileName) => {
+  // 获取组件配置
+  const componentConfig = requireComponent(fileName);
+
+  // 获取组件的 PascalCase 命名
+  const componentName = upperFirst(
+    camelCase(
+      // 获取和目录深度无关的文件名
+      fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+  // 全局注册组件
+  app.component(
+    componentName,
+    // 如果这个组件选项是通过 `export default` 导出的，
+    // 那么就会优先使用 `.default`，
+    // 否则回退到使用模块的根。
+    componentConfig.default || componentConfig
+  );
+});
+
+app.use(store).use(router).mount("#app");
--- my_vue_project/src/router/index.js
+++ my_vue_project/src/router/index.js
@@ -1,25 +1,46 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createApp } from "vue";
+const app = createApp({});
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+// 路由自动化注册
+const requireComponent = require.context("@/views", true, /\.vue$/); // 找到 views 路径下的所有文件
+
+const dynamic_route = requireComponent
+  .keys()
+  .filter((fileName) => {
+    if (fileName === "./index.vue")
+      // 过滤掉父节点的路由
+      return false;
+    else return true;
+  })
+  .map((fileName) => {
+    const componentConfig = requireComponent(fileName);
+    const componentName = fileName.replace(/^\.\//, "").replace(/\.vue$/, ""); // 剥去文件名开头的 `./` 和`.vue`结尾的扩展名
+    const componentNameRe = componentName.replace(/\//g, "-"); // 设置name为文件夹名-index
+    // 注册组件 start
+    // 注意:什么时候应用程序组件（…）提供了一个definition对象（第二个参数），它返回应用程序实例（以便允许链接调用）。
+    // 要获取组件定义，请省略定义对象并仅提供名称
+    // https://stackoverflow.com/questions/64409157/vue-js-component-is-missing-template-or-render-function
+
+    app.component(componentNameRe, componentConfig.default);
+
+    const Component = app.component(componentNameRe); // 根据路径注册成组件
+    // 注册组件 end
+    const result = {
+      path: `/${componentName}`,
+      name: componentNameRe,
+      component: Component,
+    };
+    if (result.path == "/Home") {
+      result.path = "/";
+    }
+    return result;
+  });
+
+const routes = dynamic_route;
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
